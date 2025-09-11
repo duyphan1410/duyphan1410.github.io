67371f2400821ce486933e18b6f3bab244ebd371
duyphan1410 <tienduy1410@gmail.com>
2025-09-11 14:47:37 +0700

    Add more image demo

--- js/main.js
+++ js/main.js
@@ -60,3 +60,27 @@ style.textContent = `
     }
 `;
 document.head.appendChild(style);
+
+//Swiper
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        effect: "coverflow",
+        grabCursor: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        centeredSlides: false,
+        freeMode: {
+            enabled: true,
+            sticky: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
